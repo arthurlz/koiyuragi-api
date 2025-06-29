a56aea769ac5b6ee4d0658f09728bdb7ba7af76e
Arthurlz <litar929@gmail.com>
2025-06-29 22:57:58 +0900

    add authed db client

--- src/app/lib/supabase/index.ts
+++ src/app/lib/supabase/index.ts
@@ -3,3 +3,15 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(process.env.SUPABASE_PROJECT_URL!,
   process.env.SUPABASE_ANON_KEY!)
+
+export const createAuthDb = (jwt: string) => {
+  return createClient(process.env.SUPABASE_PROJECT_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+  {
+    global: {
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      }
+    }
+  })
+}
